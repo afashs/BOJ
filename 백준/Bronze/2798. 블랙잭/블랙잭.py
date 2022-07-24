fccf5cdfd8c310e8612fe1070819b331816e4672
서정욱 [marvin] <56165665+afashs@users.noreply.github.com>
2022-07-25 00:18:33 +0900

    [Bronze II] Title: 블랙잭, Time: 128 ms, Memory: 114980 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,25 +1,19 @@
-## P
-# 21 이하
-# N장 중에서 3장
-# M <= 최대한 가깝게
-
-## I
-# N (3 ~ 100) M (10 ~ 300k)
-# list
-
 from sys import stdin, stdout
 from itertools import combinations
 input = lambda: stdin.readline()
 print = lambda o: stdout.write(str(o))
-res: list = []
 
 def solution(N: int, M: int, li: list):
+  res: int = 0
   for triple in combinations(li, 3):
-    if (sum(triple) > M):
+    sumT = sum(triple)
+    if (sumT > M):
       continue
+    elif (sumT > res):
+      res = sumT
     else:
-      res.append(sum(triple))
-  print(max(res))
+      continue
+  print(res)
 
 if (__name__ == "__main__") :
   N, M = map(int, input().split())
